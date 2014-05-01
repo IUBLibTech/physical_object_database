0a5013645a7795772733e0647b3dbcbe11e82d93
Andrew Albrecht <jaalbrec@indiana.edu>
2014-05-01 15:00:22 -0400

    removed physical_object_validator.rb and the picklist_specification_form partial

--- app/models/physical_object_validator.rb
+++
@@ -1,9 +0,0 @@
-class PhysicalObjectValidator < ActiveModel::Validator
-
-	def validate(po)
-		if po.mdpi_barcode != 0 and !ApplicationHelper.valid_barcode?(po.mdpi_barcode)
-			po.errors.add :mdpi_barcode, "is not valid!"
-		end	
-	end
-
-end
