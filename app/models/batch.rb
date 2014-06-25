c04c51e1251e87a24ffb5dbd5c22b991489f6721
Adam Ploshay <aploshay@iu.edu>
2014-06-25 17:16:05 -0400

    Fixed rspec tests for physical object model, refactored some workflow status template handling

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,14 +1,10 @@
 class Batch < ActiveRecord::Base
 	has_many :bins
 	has_many :workflow_statuses, :dependent => :destroy
-	
+
+	after_create :assign_default_workflow_status
 	include WorkflowStatusModule
 
 	validates :identifier, presence: true, uniqueness: true
-	after_create :init
 
-	def init
-		default_status = WorkflowStatusQueryModule.default_status(self)
-    self.workflow_statuses << default_status
-	end
 end
