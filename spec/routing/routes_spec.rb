29dbc6b4d37c61c3390b0c1843fe1169ca5f4dbe
Adam Ploshay <aploshay@iu.edu>
2014-08-06 11:28:59 -0400

    Added unit model spec, updated routing spec; updated Gemfile.lock

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -5,3 +5,22 @@ describe "root" do
     expect(get("/")).to route_to("physical_objects#index")
   end
 end
+
+describe "boxes" do
+  it "does not route for box/{id}/edit" do
+    expect(get("/boxes/1/edit")).not_to be_routable
+  end
+end
+
+describe "signin" do
+  it "routes to sessions#new" do
+    expect(get("/signin")).to route_to("sessions#new")
+  end
+end
+
+describe "signout" do
+  it "routes to sessions#destroy" do
+    expect(delete("/signout")).to route_to("sessions#destroy")
+  end
+end
+
