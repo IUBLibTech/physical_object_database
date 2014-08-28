7bd9659cde9d65782f1dcab128b3b6c61578b06b
Adam Ploshay <aploshay@iu.edu>
2014-08-28 18:28:36 -0400

    uncommented open reel human field names (from testing)

--- app/models/open_reel_tm.rb
+++ app/models/open_reel_tm.rb
@@ -4,13 +4,13 @@ class OpenReelTm < ActiveRecord::Base
 	extend TechnicalMetadatumClassModule
 
 	# this hash holds the human reable attribute name for this class
-	#HUMANIZED_COLUMNS = {:zero_point9375_ips => "0.9375 ips", :one_point875_ips => "1.875 ips", 
-	#	:three_point75_ips => "3.75 ips", :seven_point5_ips => "7.5 ips", :fifteen_ips => "15 ips", 
-		#:thirty_ips => "30 ips", :unknown_track => "Unknown", :zero_point5_mils => "0.5 mil",
-		#:one_mils => "1 mil", :one_point5_mils => "1.5 mil", :unknown_sound_field => "Unknown", 
-		#:acetate_base => "Acetate", :polyester_base => "Polyester", :pvc_base => "PVC", :paper_base => "Paper",
-		#:unknown_playback_speed => "Unknown", :one_direction => "1", :two_directions => "2", :unknown_direction => "Unknown" 
-	#}
+	HUMANIZED_COLUMNS = {:zero_point9375_ips => "0.9375 ips", :one_point875_ips => "1.875 ips", 
+		:three_point75_ips => "3.75 ips", :seven_point5_ips => "7.5 ips", :fifteen_ips => "15 ips", 
+		:thirty_ips => "30 ips", :unknown_track => "Unknown", :zero_point5_mils => "0.5 mil",
+		:one_mils => "1 mil", :one_point5_mils => "1.5 mil", :unknown_sound_field => "Unknown", 
+		:acetate_base => "Acetate", :polyester_base => "Polyester", :pvc_base => "PVC", :paper_base => "Paper",
+		:unknown_playback_speed => "Unknown", :one_direction => "1", :two_directions => "2", :unknown_direction => "Unknown" 
+	}
 	PLAYBACK_SPEED_FIELDS = [
           "zero_point9375_ips",
           "one_point875_ips",
