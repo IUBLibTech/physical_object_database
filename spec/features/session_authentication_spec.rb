437d9f594bcfae0b4313e45b2e25ec77a0f7866c
Adam Ploshay <aploshay@iu.edu>
2014-09-08 18:06:37 -0400

    added basic user, authentication specs

---
+++ spec/features/session_authentication_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+feature "Session authentication" do 
+
+  context "unauthenticated user" do
+    before(:each) { sign_out }
+    scenario "cannot access the site root" do
+      #NOTE: the rack test server cannot visit external URLs, so a redirect to CAS raises an error
+      sign_in(nil)
+      expect{ visit root_path }.to raise_error(ActionController::RoutingError, /No route matches/)
+    end
+  end
+
+  context "signed-in user" do  
+    before(:each) { sign_in }
+    scenario "can access the site root" do
+      expect{ visit root_path }.not_to raise_error
+    end
+  end
+
+end
