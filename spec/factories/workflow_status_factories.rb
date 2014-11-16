6f51201e580dd946da4bd31488ca639daf7270cf
Adam Ploshay <aploshay@iu.edu>
2014-11-16 12:12:41 -0500

    added trait to workflow status factories to set object type filter on template selection

--- spec/factories/workflow_status_factories.rb
+++ spec/factories/workflow_status_factories.rb
@@ -1,8 +1,19 @@
+# requires a trait to assign the proper workflow template:
+#   :bin, :batch, or :physical_object
 FactoryGirl.define do
 
   factory :workflow_status, class: WorkflowStatus do
     # select an existing workflow status template as these are seed data
-    workflow_status_template_id { WorkflowStatusTemplate.all[rand(WorkflowStatusTemplate.all.size)].id }
+    trait :batch do
+      workflow_status_template_id { WorkflowStatusTemplate.where(object_type: "Batch")[rand(WorkflowStatusTemplate.where(object_type: "Batch").size)].id }
+    end
+    trait :bin do
+      workflow_status_template_id { WorkflowStatusTemplate.where(object_type: "Bin")[rand(WorkflowStatusTemplate.where(object_type: "Bin").size)].id }
+    end
+    trait :physical_object do
+      workflow_status_template_id { WorkflowStatusTemplate.where(object_type: "Physical Object")[rand(WorkflowStatusTemplate.where(object_type: "Physical Object").size)].id }
+    end
+
     notes "Factory Girl workflow status"
   end
 
