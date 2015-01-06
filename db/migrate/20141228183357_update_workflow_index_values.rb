9c51549a91c2a17653d5d60c5f0aef5770538e2d
Adam Ploshay - LETRS hourly <aploshay@sycamore.dlib.indiana.edu>
2015-01-06 16:46:10 -0500

    Server-side hotfix for workflow index update breaking when workflow_statuses is missing status history entries

--- db/migrate/20141228183357_update_workflow_index_values.rb
+++ db/migrate/20141228183357_update_workflow_index_values.rb
@@ -4,6 +4,10 @@ class UpdateWorkflowIndexValues < ActiveRecord::Migration
     update_objects = PhysicalObject.where(workflow_index: nil) + Bin.where(workflow_index: nil) + Batch.where(workflow_index: nil)
     update_objects.each do |object|
       if object.workflow_index.nil?
+        if object.workflow_statuses.empty?
+          workflow_status_template = WorkflowStatusTemplate.find_by(name: object.workflow_status, object_type: object.class_title)
+          object.workflow_statuses.new(workflow_status_template_id: workflow_status_template.id)
+        end
         object.workflow_index = object.workflow_statuses.last.workflow_status_template.sequence_index
         object.save
       end
