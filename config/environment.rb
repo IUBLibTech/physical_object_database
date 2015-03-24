c7146a550ab14d35f6bd6102eb937ca3ee80ba0b
Adam Ploshay <aploshay@iu.edu>
2015-03-24 09:30:05 -0400

    [POD-635] Added display of version number from git tag/hash

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Use tag or commit hash as version string
+APP_VERSION = `git describe --always` unless defined? APP_VERSION
+
 # Initialize the Rails application.
 Pod::Application.initialize!
