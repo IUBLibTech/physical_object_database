5c1ada5a88b903d38ff0bb038cb1b2b744fd10df
Adam Ploshay <aploshay@iu.edu>
2015-09-15 15:11:11 -0400

    [POD-990] Added new Batch workflow status: Interim Storage

---
+++ db/migrate/20150915190311_update_batch_workflow_status_templates.rb
@@ -0,0 +1,25 @@
+class UpdateBatchWorkflowStatusTemplates < ActiveRecord::Migration
+  def up
+    puts "Adding new workflow status"
+    WorkflowStatusTemplate.create(name: 'Interim Storage', description: 'The batch has been returned from the digitization facility, but not yet entered receiving area.', sequence_index: 4, object_type: 'Batch')
+    returned = WorkflowStatusTemplate.find_by(object_type: 'Batch', name: 'Returned')
+    complete = WorkflowStatusTemplate.find_by(object_type: 'Batch', name: 'Complete')
+    returned.sequence_index = 5
+    returned.description = 'The batch has been returned from the digitization facility, and entered receiving area.'
+    returned.save!
+    complete.sequence_index = 6
+    complete.save!
+  end
+  def down
+    puts "Removing new workflow status"
+    interim = WorkflowStatusTemplate.find_by(object_type: 'Batch', name: 'Interim Storage')
+    interim.destroy!
+    returned = WorkflowStatusTemplate.find_by(object_type: 'Batch', name: 'Returned')
+    complete = WorkflowStatusTemplate.find_by(object_type: 'Batch', name: 'Complete')
+    returned.sequence_index = 4
+    returned.description = 'The batch has been returned from the digitization facility.'
+    returned.save!
+    complete.sequence_index = 5
+    complete.save!
+  end
+end
