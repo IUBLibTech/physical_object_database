6c6566bdea897a682bd81b847a2a158ff30cd92c
Andrew Albrecht <jaalbrec@indiana.edu>
2015-09-22 21:19:46 -0400

    the schema...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921124631) do
+ActiveRecord::Schema.define(version: 20150917141859) do
 
   create_table "analog_sound_disc_tms", force: true do |t|
     t.string   "diameter"
