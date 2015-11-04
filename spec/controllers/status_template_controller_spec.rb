80f16e4eeee59a8f0487fb8086179b38380db8a8
Adam Ploshay <aploshay@iu.edu>
2015-11-04 17:20:27 -0500

    [POD-382] [POD-1071] Added status_template_controller spec

--- spec/controllers/status_template_controller_spec.rb
+++ spec/controllers/status_template_controller_spec.rb
@@ -1,3 +1,14 @@
 describe StatusTemplatesController do
-  pending "write tests"
+  render_views
+  before(:each) { sign_in }
+
+  describe "GET #index" do
+    before(:each) { get :index }
+    it "assigns all_workflow_status_templates" do
+      expect(assigns(:all_workflow_status_templates).values.flatten.size).to eq WorkflowStatusTemplate.all.size
+    end
+    it "assigns all_condition_status_templates" do
+      expect(assigns(:all_condition_status_templates).values.flatten.size).to eq ConditionStatusTemplate.all.size
+    end
+  end
 end
