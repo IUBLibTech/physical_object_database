016d1d6002ebe4eedcd85dacdee0c40c2005496e
Adam Ploshay <adam.ploshay@gmail.com>
2015-11-06 17:11:22 -0500

    [POD-382] [POD-1071] Added processing_steps_controller specs

--- spec/controllers/processing_steps_controller_spec.rb
+++ spec/controllers/processing_steps_controller_spec.rb
@@ -2,8 +2,21 @@ describe ProcessingStepsController do
   render_views
   before(:each) { sign_in; request.env['HTTP_REFERER'] = 'source_page' }
 
+  let!(:processing_step) { FactoryGirl.create :processing_step }
+
   describe "DELETE #destroy" do
-    pending
+    let(:delete_destroy) { delete :destroy, id: processing_step.id }
+    it "assigns the requested object" do
+      delete_destroy
+      expect(assigns(:processing_step)).to eq processing_step
+    end
+    it "destroys the requested object" do
+      expect { delete_destroy }.to change(ProcessingStep, :count).by(-1)
+    end
+    it "redirects to :back" do
+      delete_destroy
+      expect(response).to redirect_to "source_page"
+    end
   end
 
 end
