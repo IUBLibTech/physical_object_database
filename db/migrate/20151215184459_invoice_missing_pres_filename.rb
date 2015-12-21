ee52f3519560daea6d8a5bcd0d7cf0475a6fe3b3
Adam Ploshay <aploshay@iu.edu>
2015-12-21 11:18:27 -0500

    [POD-1124] Modified migration to allow reversal

--- db/migrate/20151215184459_invoice_missing_pres_filename.rb
+++ db/migrate/20151215184459_invoice_missing_pres_filename.rb
@@ -1,9 +1,9 @@
 class InvoiceMissingPresFilename < ActiveRecord::Migration
   def change
-    remove_column :memnon_invoice_submissions, :already_billed
-    remove_column :memnon_invoice_submissions, :not_found
-    remove_column :memnon_invoice_submissions, :not_on_sda
-    remove_column :memnon_invoice_submissions, :preservation_file_copies
+    remove_column :memnon_invoice_submissions, :already_billed, :text, limit: 4294967295
+    remove_column :memnon_invoice_submissions, :not_found, :text, limit: 4294967295
+    remove_column :memnon_invoice_submissions, :not_on_sda, :text, limit: 4294967295
+    remove_column :memnon_invoice_submissions, :preservation_file_copies, :text, limit: 4294967295
     add_column :memnon_invoice_submissions, :problems_by_row, :text, limit: 4294967295
   end
 end
