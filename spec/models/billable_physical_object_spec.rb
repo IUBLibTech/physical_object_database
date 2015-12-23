9a02a7e1d9130223f9dc7fa2b5903d07f099d282
Adam Ploshay <aploshay@iu.edu>
2015-12-23 14:59:43 -0500

    [POD-382] [POD-1075] Added BillablePhysicalObject specs

--- spec/models/billable_physical_object_spec.rb
+++ spec/models/billable_physical_object_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
-
-RSpec.describe BillablePhysicalObject, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe BillablePhysicalObject do
+  let(:valid_bpo) { FactoryGirl.build :billable_physical_object }
+  describe "FactoryGirl" do
+    it "returns a valid object" do
+    end
+  end
+  describe "has optional fields:" do
+    specify "mdpi_barcode" do
+      valid_bpo.mdpi_barcode = nil
+      expect(valid_bpo).to be_valid
+    end
+    specify "delivery_date" do
+      valid_bpo.delivery_date = nil
+      expect(valid_bpo).to be_valid
+    end
+  end
 end
