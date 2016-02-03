b34501be516e4ee526ffe6eb7590cdc5588e83a8
Andrew Albrecht <jaalbrec@indiana.edu>
2016-02-03 15:47:36 -0500

    [POD-1190] - Redirect after resquesting staging working... I think

--- app/controllers/quality_control_controller.rb
+++ app/controllers/quality_control_controller.rb
@@ -4,10 +4,6 @@ class QualityControlController < ApplicationController
 	before_action :set_iu_staging, only: [:iu_staging_index]
 
 	def index
-		# This call is necessary as it initializes any staging percentages for formats that are present in the POD but not
-		# present in the staging percentages table.
-		StagingPercentagesController::validate_formats
-
 		if params[:status]
 			@physical_objects = DigitalStatus.current_actionable_status(params[:status])
 			ActiveRecord::Associations::Preloader.new.preload(@physical_objects, [:unit, :digital_statuses])
@@ -15,10 +11,16 @@ class QualityControlController < ApplicationController
 	end
 
 	def iu_staging_index
+		# This call is necessary as it initializes any staging percentages for formats that are present in the POD but not
+		# present in the staging percentages table.
+		StagingPercentagesController::validate_formats
 		render 'staging'
 	end
 
 	def staging_index
+		# This call is necessary as it initializes any staging percentages for formats that are present in the POD but not
+		# present in the staging percentages table.
+		StagingPercentagesController::validate_formats
 		render 'staging'
 	end
 
@@ -35,6 +37,8 @@ class QualityControlController < ApplicationController
 	end
 
 	def stage
+		@success = false
+		@msg = ''
 		begin
 			po = PhysicalObject.find(params[:id])
 			po.update_attributes(staging_requested: true)
@@ -52,7 +56,7 @@ class QualityControlController < ApplicationController
 		@ds = DigitalStatus.find(params[:id])
 		@ds.update_attributes(decided: params[:decided], decided_manually: true)
 		flash[:notice] = "Updated Digital Status for #{@ds.physical_object.mdpi_barcode} - chose #{@ds.decided}"
-		render "index"
+		render 'index'
 	end
 
 	# displays auto_accept logs
@@ -61,26 +65,23 @@ class QualityControlController < ApplicationController
 
 	def self.percent(format, entity)
 		sp = StagingPercentage.where(format: format).first
-		entity == "Memnon" ? sp.memnon_percent / 100.0 : sp.iu_percent / 100.0
+		entity == 'Memnon' ? sp.memnon_percent / 100.0 : sp.iu_percent / 100.0
 	end
 
 	private
 	def set_header_title
-		@header_title = params[:status].nil? ? "" : params[:status].titleize
+		@header_title = params[:status].nil? ? '' : params[:status].titleize
 		authorize :quality_control
 	end
 
 	def set_staging
 		@action = 'staging_index'
-		now = Time.now
+		@date = Time.now
 		if params[:staging] && params[:staging][:date]
-			@date = params[:staging][:date].blank? ? Time.new(now.year, now.month, now.day) : DateTime.strptime(params[:staging][:date], "%m/%d/%Y")
-		else
-			@date = Time.new(now.year, now.month, now.day)
+			@date = params[:staging][:date].blank? ? now : DateTime.strptime(params[:staging][:date], '%m/%d/%Y')
 		end
-		@d_entity = "Memnon"
-
-		formats = params[:staging].blank? || params[:staging][:format] == "All" ? PhysicalObject.memnon_unstaged_by_date_formats(@date) : [] << params[:staging][:format]
+		@d_entity = 'Memnon'
+		formats = params[:staging].blank? || params[:staging][:format] == 'All' ? PhysicalObject.memnon_unstaged_by_date_formats(@date) : [] << params[:staging][:format]
 		@format_to_physical_objects = ActiveSupport::OrderedHash.new
 		formats.each do |format|
 			@format_to_physical_objects[format] = PhysicalObject.memnon_unstaged_by_date_and_format(@date, format)
@@ -88,15 +89,15 @@ class QualityControlController < ApplicationController
 	end
 
 	def set_iu_staging
-		@action = "iu_staging_index"
+		@action = 'iu_staging_index'
 		now = Time.now
 		if params[:staging] && params[:staging][:date]
 				@date = params[:staging][:date].blank? ? Time.new(now.year, now.month, now.day) : DateTime.strptime(params[:staging][:date], '%m/%d/%Y')
 		else
 			@date = Time.new(now.year, now.month, now.day)
 		end
-		@d_entity = "IU"
-		formats = params[:staging].blank? || params[:staging][:format] == "All"  ? PhysicalObject.iu_unstaged_by_date_formats(@date) : [] << params[:staging][:format]
+		@d_entity = 'IU'
+		formats = params[:staging].blank? || params[:staging][:format] == 'All' ? PhysicalObject.iu_unstaged_by_date_formats(@date) : [] << params[:staging][:format]
 		@format_to_physical_objects = ActiveSupport::OrderedHash.new
 		formats.each do |format|
 			@format_to_physical_objects[format] = PhysicalObject.iu_unstaged_by_date_and_format(@date, format)
