13ccc6db53dedf93aa3f289b244a2eb8b70789d9
Andrew Albrecht <jaalbrec@indiana.edu>
2016-03-28 15:46:25 -0400

    [POD-1298] Users can now see the metadata for physical objects in their group

--- spec/controllers/collection_owner_controller_spec.rb
+++ spec/controllers/collection_owner_controller_spec.rb
@@ -7,7 +7,26 @@ describe CollectionOwnerController do
   end
 
   describe "#show" do
-    pending "write show tests"
+    let(:good) { FactoryGirl.create( :physical_object, :cdr, mdpi_barcode: valid_mdpi_barcode, unit: Unit.first ) }
+    let(:bad) { FactoryGirl.create( :physical_object, :cdr, mdpi_barcode: valid_mdpi_barcode, unit: Unit.last ) }
+
+    # make sure the web_admin user (what rspec uses for testing) is properly unit affiliated
+    before(:each) {
+      User.find_by_username('web_admin').update_attributes(unit_id: Unit.first.id)
+    }
+
+    it 'allows access to a physical object in a users unit' do
+      get :show, id: good.id
+      expect(assigns(:physical_object)).to eq good
+      expect(response).to render_template(:show)
+    end
+
+    it 'does not allow access to a physical object from another unit than the users' do
+      get :show, id: bad.id
+      expect(assigns(:physical_object)).to eq bad
+      expect(response).to redirect_to '/collection_owner'
+    end
+
   end
 
   describe "#search" do
