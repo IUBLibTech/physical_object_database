1a8da376a50172b9b794514183b88d527e755a0f
Adam Ploshay <aploshay@iu.edu>
2016-03-28 17:47:18 -0400

    [POD-1294] Added index views (HTML, XLS) for collection_owner#index

--- app/controllers/collection_owner_controller.rb
+++ app/controllers/collection_owner_controller.rb
@@ -1,8 +1,11 @@
 class CollectionOwnerController < ApplicationController
   before_action :check_authorization
-  before_action :check_unit_affiliation, except: [:index]
 
   def index
+    @physical_objects = PhysicalObject.collection_owner_filter(@pundit_user.unit_id)
+    if request.format.html?
+      @physical_objects = @physical_objects.paginate(page: params[:page])
+    end
   end
 
   def show
@@ -19,11 +22,9 @@ class CollectionOwnerController < ApplicationController
   private
     def check_authorization
       authorize :collection_owner
-    end
-    def check_unit_affiliation
       if @pundit_user.nil? || @pundit_user.unit.nil?
         flash[:warning] = 'You must have an associated Unit to use collection owner services.'
-        redirect_to collection_owner_index_path
+        redirect_to welcome_index_path
       end
     end
 end
--- spec/controllers/collection_owner_controller_spec.rb
+++ spec/controllers/collection_owner_controller_spec.rb
@@ -5,29 +5,67 @@ describe CollectionOwnerController do
   let(:unit_bad) { Unit.last }
   let(:status_good) { 'Returned to Unit' }
   let(:status_bad) { 'Unassigned' }
-  let(:po_good) { FactoryGirl.create( :physical_object, :cdr, mdpi_barcode: valid_mdpi_barcode, unit: unit_good, workflow_status: status_good ) }
-  let(:po_bad_unit) { FactoryGirl.create( :physical_object, :cdr, mdpi_barcode: valid_mdpi_barcode, unit: unit_bad, workflow_status: status_good ) }
-  let(:po_bad_status) { FactoryGirl.create( :physical_object, :cdr, mdpi_barcode: valid_mdpi_barcode, unit: unit_good, workflow_status: status_bad ) }
+  let!(:po_good) { FactoryGirl.create( :physical_object, :cdr, mdpi_barcode: valid_mdpi_barcode, unit: unit_good, workflow_status: status_good ) }
+  let!(:po_bad_unit) { FactoryGirl.create( :physical_object, :cdr, mdpi_barcode: valid_mdpi_barcode, unit: unit_bad, workflow_status: status_good ) }
+  let!(:po_bad_status) { FactoryGirl.create( :physical_object, :cdr, mdpi_barcode: valid_mdpi_barcode, unit: unit_good, workflow_status: status_bad ) }
 
+  shared_examples "no unit affiliation" do
+    before(:each) { expect(assigns(:pundit_user).unit).to be_nil }
+    it "flashes a warning" do
+      expect(flash[:warning]).to match /must have an associated Unit/i
+    end
+    it "redirects to :index" do
+      expect(response).to redirect_to welcome_index_path
+    end
+  end
   describe "#index" do
-    pending "write index tests"
+    context "without a unit association" do
+      before(:each) { get :index }
+      include_examples "no unit affiliation"
+    end
+    context "with a unit association" do
+      before(:all) { User.find_by_username('web_admin').update_attributes(unit_id: Unit.first.id) }
+      after(:all) { User.find_by_username('web_admin').update_attributes(unit_id: nil) }
+      shared_examples "index examples" do
+        it "lists viewable objects" do
+          results = assigns(:physical_objects)
+          expect(results).to include po_good
+          expect(results).not_to include po_bad_unit
+          expect(results).not_to include po_bad_status
+        end
+        it "renders :index template" do
+          expect(response).to render_template :index
+        end
+      end
+      context "in HTML format" do
+        before(:each) { get :index }
+        include_examples "index examples"
+        it "includes pagination" do
+          expect(assigns(:physical_objects)).to respond_to(:total_pages)
+        end
+      end
+      context "in XLS format" do
+        before(:each) { get :index, format: "xls" }
+        include_examples "index examples"
+        it "does not include pagination" do
+          expect(assigns(:physical_objects)).not_to respond_to(:total_pages)
+        end
+      end
+    end
   end
 
   describe "#show" do
     context "without a unit association" do
-      before(:each) { User.find_by_username('web_admin').update_attributes(unit_id: nil) }
       before(:each) { get :show, id: po_good.id }
-      it "flashes a warning" do
-        expect(flash[:warning]).to match /must have an associated Unit/i
-      end
-      it "redirects to :index" do
-        expect(response).to redirect_to '/collection_owner'
-      end
+      include_examples "no unit affiliation"
     end
     context "with a unit association" do
       # make sure the web_admin user (what rspec uses for testing) is properly unit affiliated
-      before(:each) {
+      before(:all) {
         User.find_by_username('web_admin').update_attributes(unit_id: Unit.first.id)
+      }
+      after(:all) {
+        User.find_by_username('web_admin').update_attributes(unit_id: nil)
       }
   
       it 'allows access to a physical object in a users unit' do
