e31d14ce7a25a4d021be08357c6100f7cf3fd9b0
Adam Ploshay <aploshay@iu.edu>
2016-03-28 16:46:57 -0400

    [POD-1298] Added check for viewable workflow_status

--- app/controllers/collection_owner_controller.rb
+++ app/controllers/collection_owner_controller.rb
@@ -1,14 +1,14 @@
 class CollectionOwnerController < ApplicationController
   before_action :check_authorization
+  before_action :check_unit_affiliation, except: [:index]
 
   def index
   end
 
   def show
-    @physical_object = PhysicalObject.find(params[:id])
-    unit = Unit.find(pundit_user.unit_id)
-    unless unit.id == @physical_object.unit_id
-      flash[:warning] = "No Physical object belonging to #{unit.name} with ID: #{params[:id]}"
+    @physical_object = PhysicalObject.collection_owner_filter(@pundit_user.unit_id).where(id: params[:id]).first
+    if @physical_object.nil?
+      flash[:warning] = "No Physical object found belonging to #{@pundit_user.unit.name} with ID: #{params[:id]}"
       redirect_to collection_owner_index_path
     end
   end
@@ -19,5 +19,11 @@ class CollectionOwnerController < ApplicationController
   private
     def check_authorization
       authorize :collection_owner
+    end
+    def check_unit_affiliation
+      if @pundit_user.nil? || @pundit_user.unit.nil?
+        flash[:warning] = 'You must have an associated Unit to use collection owner services.'
+        redirect_to collection_owner_index_path
+      end
     end
 end
