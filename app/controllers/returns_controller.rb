2bd3f731ff4c46d04fd8df4b18622b68e317f71e
Andrew Albrecht <jaalbrec@indiana.edu>
2016-07-18 14:59:32 -0400

    backup

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -33,9 +33,11 @@ class ReturnsController < ApplicationController
 
 		# GOOD! - we have a physical object that can actually be returned
 		else
+			debugger
 			if @po.has_ephemera
 				# refuse to return a physical object unless the ephemera returned flag has explicitly been set
-				if params[:ephemera_returned].blank? or (params[:ephemera_returned] != 'true' or params[:ephemera_returned] != false)
+				debugger
+				if params[:ephemera_returned].blank? or (params[:ephemera_returned] != 'true' and params[:ephemera_returned] != 'false')
 					flash[:warning] = 'Ephemera returned was not specified - Physical Object <b>Not</b> unpacked'.html_safe
 				else
 					@po.update_attributes(current_workflow_status: "Unpacked", ephemera_returned: params[:ephemera_returned])
