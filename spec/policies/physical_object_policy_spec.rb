0e87edcd2dd882a6a540aa849a0de2e384e60ffc
Adam Ploshay <aploshay@iu.edu>
2018-03-12 14:51:06 -0400

    [circle_ci] again

--- spec/policies/physical_object_policy_spec.rb
+++ spec/policies/physical_object_policy_spec.rb
@@ -1,7 +1,7 @@
 describe PhysicalObjectPolicy do
   let(:policy) { PhysicalObjectPolicy.new(user, physical_object) }
   let(:physical_object) { FactoryBot.create :physical_object, :cdr }
-  WEB_ADMIN = User.where(username: "web_admin").first
+  WEB_ADMIN = User.find_or_create_by(username: 'web_admin', name: 'web_admin', web_admin: true)
 
   context "web_admin user" do
     let(:user) { WEB_ADMIN }
