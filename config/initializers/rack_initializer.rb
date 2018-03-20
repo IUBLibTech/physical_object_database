4a8fb0f0ac8a584a7f0b0b64f1fed49d3322b57a
Adam Ploshay <aploshay@iu.edu>
2018-03-20 14:19:18 -0400

    [POD-1648] Increase key_space_limit

--- config/initializers/rack_initializer.rb
+++ config/initializers/rack_initializer.rb
@@ -1,4 +1,4 @@
-# Increase key_space_limit to 8x default of 65536, for filmdb XML pushes
+# Increase key_space_limit to 48x default of 65536, for filmdb XML pushes
 if Rack::Utils.respond_to?("key_space_limit=")
-  Rack::Utils.key_space_limit = 524288
+  Rack::Utils.key_space_limit = 3145728
 end
