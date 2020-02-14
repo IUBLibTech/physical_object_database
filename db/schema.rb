237ed745b4e05d9057c6f2577a01e235bc47aed7
Adam Ploshay <aploshay@iu.edu>
2020-02-14 12:26:58 -0500

    [POD-1736] Remove stats tables from schema

--- db/schema.rb
+++ db/schema.rb
@@ -322,33 +322,6 @@ ActiveRecord::Schema.define(version: 20200129210006) do
   add_index "digital_statuses", ["created_at", "state", "physical_object_id"], name: "quality_control_staging", using: :btree
   add_index "digital_statuses", ["physical_object_id"], name: "index_digital_statuses_on_physical_object_id", using: :btree
 
-  create_table "doFiles", id: false, force: :cascade do |t|
-    t.string  "mdpiBarcode", limit: 14,  null: false
-    t.integer "partNumber",  limit: 1
-    t.boolean "isMaster"
-    t.string  "fileUsage",   limit: 255
-    t.string  "md5",         limit: 32
-    t.integer "size",        limit: 8
-    t.float   "duration",    limit: 24
-  end
-
-  add_index "doFiles", ["fileUsage"], name: "doFiles_fileUsage", using: :btree
-  add_index "doFiles", ["mdpiBarcode", "partNumber"], name: "mdpiBarcode", using: :btree
-
-  create_table "doObjects", primary_key: "mdpiBarcode", force: :cascade do |t|
-    t.string   "digitizingEntity", limit: 255
-    t.string   "objectType",       limit: 255
-    t.datetime "acceptTime"
-    t.datetime "bagTime"
-    t.integer  "size",             limit: 8
-  end
-
-  create_table "doParts", id: false, force: :cascade do |t|
-    t.string  "mdpiBarcode", limit: 14, null: false
-    t.integer "partNumber",  limit: 1,  null: false
-    t.boolean "vendorQC"
-  end
-
   create_table "dv_tms", force: :cascade do |t|
     t.string   "recording_standard",     limit: 255
     t.string   "format_duration",        limit: 255
@@ -928,39 +901,6 @@ ActiveRecord::Schema.define(version: 20200129210006) do
   add_index "workflow_statuses", ["bin_id", "workflow_status_template_id"], name: "index_ws_on_bin_and_wst", using: :btree
   add_index "workflow_statuses", ["physical_object_id", "workflow_status_template_id"], name: "index_ws_on_po_and_wst", using: :btree
 
-  create_table "xDigitizingEntity", force: :cascade do |t|
-    t.string   "diameter",           limit: 255
-    t.string   "speed",              limit: 255
-    t.string   "groove_size",        limit: 255
-    t.string   "groove_orientation", limit: 255
-    t.string   "recording_method",   limit: 255
-    t.string   "material",           limit: 255
-    t.string   "substrate",          limit: 255
-    t.string   "coating",            limit: 255
-    t.string   "equalization",       limit: 255
-    t.string   "country_of_origin",  limit: 255
-    t.boolean  "delamination"
-    t.boolean  "exudation"
-    t.boolean  "oxidation"
-    t.boolean  "cracked"
-    t.boolean  "warped"
-    t.boolean  "dirty"
-    t.boolean  "scratched"
-    t.boolean  "worn"
-    t.boolean  "broken"
-    t.boolean  "fungus"
-    t.string   "label",              limit: 255
-    t.string   "sound_field",        limit: 255
-    t.string   "subtype",            limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "xState", force: :cascade do |t|
-    t.string "state", limit: 255
-  end
-
-  add_foreign_key "doParts", "doObjects", column: "mdpiBarcode", primary_key: "mdpiBarcode", name: "doParts_ibfk_1"
   add_foreign_key "physical_objects", "shipments"
   add_foreign_key "picklists", "shipments"
   add_foreign_key "shipments", "units"
