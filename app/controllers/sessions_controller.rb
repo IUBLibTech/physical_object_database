7483b2cb4280e118e34e9b4a23f4f03003c50887
Adam Ploshay <aploshay@iu.edu>
2021-05-19 16:10:35 -0400

    [POD-1783] Slight refactor for IU Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,37 +1,36 @@
 # Does not inherit from ApplicationController to avoid requiring sign-in here
 class SessionsController < ActionController::Base
   require 'net/http'
-
-  def cas
-    # FIXME: handle real case
-    "https://idp-stg.login.iu.edu/idp/profile"
-  end
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  before_action :cas_ticket, only: [:new, :validate_login]
+  before_action :cas_user, only: [:validate_login]
 
-  def new
-    params[:ticket].present? ? validate_login : get_ticket
+  def cas
+    Pod.config[:cas_url]
   end
 
-  def get_ticket
-    redirect_to("#{cas}/cas/login?service=#{root_url}")
+  def ticket_url(service)
+    "#{cas}/login?service=#{service}"
+  end
+
+  def validation_url(ticket, service)
+    "#{cas}/serviceValidate?ticket=#{ticket}&service=#{service}"
+  end
+
+  def logout_url
+    "#{cas}/logout"
+  end
+
+  def new
+    @cas_ticket.present? ? validate_login : redirect_to(ticket_url(root_url))
   end
 
   def validate_login
-    @casticket=params[:ticket]
-    uri = URI.parse("#{cas}/cas/serviceValidate?ticket=#{@casticket}&service=#{root_url}")
-    request = Net::HTTP.new(uri.host, uri.port)
-    request.use_ssl = true
-    request.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    response = request.get("#{cas}/cas/serviceValidate?ticket=#{@casticket}&service=#{root_url}")
-    @resp = response.body
-    @xml = Nokogiri::XML(@resp)
-    @resp_user = @xml.xpath('/cas:serviceResponse/cas:authenticationSuccess/cas:user').text
-    if @resp_user.present? && User.authenticate(@resp_user)
-      sign_in(@resp_user) 
+    if @cas_user.present? && User.authenticate(@cas_user)
+      sign_in(@cas_user) 
       redirect_back_or_to root_url
     else
       redirect_to "#{root_url}denied.html"
@@ -40,6 +39,26 @@ class SessionsController < ActionController::Base
 
   def destroy
     sign_out
-    redirect_to "#{cas}/cas/logout"
+    redirect_to(logout_url)
   end
+
+  private
+    def cas_ticket
+      @cas_ticket = params[:ticket]
+    end
+
+    def cas_user
+      begin
+        uri = URI.parse(validation_url(@cas_ticket, root_url))
+        request = Net::HTTP.new(uri.host, uri.port)
+        request.use_ssl = true
+        request.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        response = request.get(uri.to_s)
+        @resp = response.body
+        @xml = Nokogiri::XML(@resp)
+        @cas_user = @xml.xpath('/cas:serviceResponse/cas:authenticationSuccess/cas:user').text
+      rescue
+        @cas_user ||= ''
+      end
+    end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -15,56 +15,93 @@ describe SessionsController do
        </cas:authenticationFailure>
      </cas:serviceResponse>' }
 
-  describe "#cas" do
-    it "returns a cas URL" do
-      expect(SessionsController.new.cas).to match /https.*login/
+  describe "url helpers" do
+    describe "#cas" do
+      it "returns a cas service root URL" do
+        expect(SessionsController.new.cas).to match /https.*login/
+      end
+    end
+  
+    describe "#ticket_url" do
+      it "returns a service URL to request a ticket" do
+        expect(SessionsController.new.ticket_url('')).to match /login.*service/
+      end
+    end
+  
+    describe "#validation_url" do
+      it "returns a service URL to validate a specific ticket" do
+        expect(SessionsController.new.validation_url('', '')).to match /login.*validate.*ticket/i
+      end
+    end
+
+    describe "#logout_url" do
+      it "returns a service URL to log out of CAS" do
+        expect(SessionsController.new.logout_url).to match /login.*logout/i
+      end
     end
   end
 
   describe "#new" do
     before(:each) do
-      stub_request(:get, /idp-stg.login.iu.edu/).
+      stub_request(:get, /idp.login.iu.edu/).
         with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
         to_return(status: 200, body: success, headers: {})
     end
     before(:each) { get :new }
-    let(:cas) { "https://idp-stg.login.iu.edu/idp/profile" }
     it "redirects to cas login" do
-      expect(response).to redirect_to "#{cas}/cas/login?service=#{root_url}"
+      expect(response).to redirect_to SessionsController.new.ticket_url(root_url)
     end
   end
   describe "#validate_login" do
-    context "with invalid casticket" do
+    context "with invalid ticket" do
       before(:each) do
-        stub_request(:get, /idp-stg.login.iu.edu/).
+        stub_request(:get, /idp.login.iu.edu/).
           with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
           to_return(status: 200, body: failure, headers: {})
       end
-      before(:each) { get :validate_login, ticket: casticket }
-      let(:casticket) { 'foo' }
-      it "assigns @casticket" do
-        expect(assigns(:casticket)).to eq casticket
+      before(:each) { get :validate_login, ticket: cas_ticket }
+      let(:cas_ticket) { 'foo' }
+      it "assigns @cas_ticket" do
+        expect(assigns(:cas_ticket)).to eq cas_ticket
       end
-      it "assigns @resp_user blank" do
-        expect(assigns(:resp_user)).to be_blank
+      it "assigns @cas_user blank" do
+        expect(assigns(:cas_user)).to be_blank
       end
       it "redirects to denial page" do
         expect(response).to redirect_to "#{root_url}denied.html"
       end
     end
-    context "with valid casticket" do
+    context "with invalid response" do
       before(:each) do
-        stub_request(:get, /idp-stg.login.iu.edu/).
+        stub_request(:get, /idp.login.iu.edu/).
+          with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+          to_return(status: 200, body: 'non-parsing response', headers: {})
+      end
+      before(:each) { get :validate_login, ticket: cas_ticket }
+      let(:cas_ticket) { 'foo' }
+      it "assigns @cas_ticket" do
+        expect(assigns(:cas_ticket)).to eq cas_ticket
+      end
+      it "assigns @cas_user blank" do
+        expect(assigns(:cas_user)).to be_blank
+      end
+      it "redirects to denial page" do
+        expect(response).to redirect_to "#{root_url}denied.html"
+      end
+    end
+    context "with valid ticket" do
+      before(:each) do
+        stub_request(:get, /idp.login.iu.edu/).
           with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
           to_return(status: 200, body: success, headers: {})
       end
-      before(:each) { get :validate_login, ticket: casticket }
-      let(:casticket) { 'valid' }
-      it "assigns @casticket" do
-        expect(assigns(:casticket)).to eq casticket
+      before(:each) { get :validate_login, ticket: cas_ticket }
+      let(:cas_ticket) { 'valid' }
+      it "assigns @cas_ticket" do
+        expect(assigns(:cas_ticket)).to eq cas_ticket
       end
-      it "assigns @resp_user" do
-        expect(assigns(:resp_user)).not_to be_blank
+      it "assigns @cas_user" do
+        expect(assigns(:cas_user)).not_to be_blank
       end
       it "redirects to root_url" do
         expect(response).to redirect_to root_url
@@ -82,7 +119,7 @@ describe SessionsController do
     end
     it "redirects to logout service'" do
       destroy_session
-      expect(response).to redirect_to 'https://idp-stg.login.iu.edu/idp/profile/cas/logout'
+      expect(response).to redirect_to SessionsController.new.logout_url
     end
   end
   
