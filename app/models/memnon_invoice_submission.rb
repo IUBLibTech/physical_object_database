ab212123c040975d4d1e9ffee03201d4abd01070
Adam Ploshay <aploshay@iu.edu>
2022-07-21 15:47:01 -0400

    [POD-1795] deprecate old serialized fields

--- app/models/memnon_invoice_submission.rb
+++ app/models/memnon_invoice_submission.rb
@@ -1,5 +1,4 @@
 class MemnonInvoiceSubmission < ActiveRecord::Base
-	serialize :problems_by_row
-	serialize :problems_by_row_json, JSON
+	serialize :problems_by_row, JSON
 	validates :filename, presence: true
 end
